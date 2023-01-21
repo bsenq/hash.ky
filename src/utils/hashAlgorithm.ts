f48d4862da38a564463200343c0bb982521c425e
theMarzon <themarzon@gmail.com>
2023-01-21 16:42:44 -0400

    🎨 Codigo refactorizado

--- src/utils/hashAlgorithm.ts
+++ src/utils/hashAlgorithm.ts
@@ -1,13 +1,19 @@
 export default function (characters: string) {
 
-    const base = 31;
+    const limit = Number.MAX_SAFE_INTEGER;
+
+    const base = 13;
 
     let hash = 0;
 
     for (let char = 0; char < characters.length; char++) {
 
-        hash += characters.charCodeAt(char) * (base ** char);
+        hash *= base;
+
+        hash += characters.charCodeAt(char);
+
+        hash %= limit;
     };
 
-    return hash % Number.MAX_SAFE_INTEGER;
+    return hash;
 };
